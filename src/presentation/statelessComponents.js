69d14e59fbbf4f24272791b70f6a8ad8ca24da11
hdennen <h@harrydennen.com>
2018-05-05 10:39:53 +0200

    color code up or down candle data

--- src/presentation/statelessComponents.js
+++ src/presentation/statelessComponents.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 function PropertyValueDisplay(props) {
     return (
-        <div>
+        <div className="key-value-element">
             {props.property}: {props.value}
         </div>
     )
 }
 
 function CandleData(props) {
+    const candleClass = 'candle-data';
+    const colorClass = props.candleData.deltaPrice > 0 ? 'up-candle' : 'down-candle';
+
     return (
-        <div>
+        <div className={[candleClass, colorClass].join(' ')}>
             {Object.keys(props.candleData).map((property, index) => {
                 return <PropertyValueDisplay key={index} property={property} value={props.candleData[property]}/>;
             })}
